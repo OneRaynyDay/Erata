1a59d1a637ddc087794112bf5ac44599de452e7f
Ray Zhang <ray.zhang@airbnb.com>
2020-05-25 13:19:07 -0700

    Crazy TMP stuff going on

--- include/alloc.hpp
+++ include/alloc.hpp
@@ -17,9 +17,9 @@
 namespace erata {
 
 // Keep this many records in the buffer
-static const int record_buffer_size = 100;
+static constexpr int record_buffer_size = 100;
 // Global namespace for now when we don't deal w/ thread safety.
-static const std::string GLOBAL_SCOPE = "global";
+static constexpr auto GLOBAL_SCOPE = "global";
 
 /// Statically managed state objects for allocators which are trivially copyable and movable.
 /// TODO: Make this thread-safe. This may require a singleton-per-thread thread_local storage design.
@@ -58,9 +58,6 @@ class profile_state {
     std::array<record, record_buffer_size> record_buffer;
     int record_buffer_counter;
 
-    /// Singleton containing state necessary for all profile_allocators
-    static profile_state state;
-
 public:
     // Following clang-tidy's rules to move this to public
     // Disallow copying/assigning/moving actual resources
@@ -69,8 +66,26 @@ public:
     profile_state(const profile_state&) = delete;
     profile_state& operator=(const profile_state&) = delete;
 
+    /// Return the singleton profile_state.
     static profile_state& get_state() {
+        /// Singleton containing state necessary for all profile_allocators
+        static profile_state state;
         return state;
+    }
+
+    /// Push a string context onto the existing scope. This is useful for
+    /// Isolating parts of subroutines where extensive dynamic allocation
+    /// is being done. In the final output, we should expect the allocations
+    /// to be recorded with this tag.
+    static void push_scope(const std::string& s) {
+        get_state().scopes.push(s);
+    }
+
+    /// Pops the scope from the stack and returns the context to the user.
+    static std::string pop_scope() {
+        auto s = get_state().scopes.top();
+        get_state().scopes.pop();
+        return s;
     }
 
 private:
@@ -80,64 +95,95 @@ private:
     }
 };
 
+// Exposing the functions to ert:: namespace for ease of use.
+static constexpr auto push_scope = &profile_state::push_scope;
+static constexpr auto pop_scope = &profile_state::pop_scope;
+
 template<typename T, typename base_allocator=std::allocator<T>>
 class profile_allocator {
 public:
-    /// Necessary for allocators
-    using value_type = T;
+    /// Necessary for allocators, propagate exactly what the base_allocator
+    /// wants.
+    typedef typename std::allocator_traits<base_allocator>::size_type size_type;
+    typedef typename std::allocator_traits<base_allocator>::difference_type difference_type;
+    typedef typename std::allocator_traits<base_allocator>::pointer pointer_type;
+    typedef typename std::allocator_traits<base_allocator>::const_pointer const_pointer;
+    typedef typename std::allocator_traits<base_allocator>::reference reference;
+    typedef typename std::allocator_traits<base_allocator>::const_reference const_reference;
+    typedef typename std::allocator_traits<base_allocator>::value_type value_type;
 
-    // TODO: fill in
+    template <class U>
+    struct rebind {
+        typedef profile_allocator<U,
+                                  typename std::allocator_traits<base_allocator>::template rebind_alloc<U>> other;
+    };
+
+    /// Forward all necessary arguments to the nested allocator.
+    template <typename... Args>
+    profile_allocator(Args&&... args) noexcept : alloc(std::forward<Args>(args)...) {}
+
+    /// Upon copy construction, we must guarrantee that it must be the base allocator
+    /// but we allow it to be any type U templatized by base_allocator. To do this we
+    /// unfortunately need to use rebind_alloc.
+    template <typename U>
+    constexpr profile_allocator(const profile_allocator<U,
+            typename std::allocator_traits<base_allocator>::template rebind_alloc<U>>&) noexcept {}
+
+    [[nodiscard]] T* allocate(std::size_t n) {
+        return alloc.allocate(n);
+    }
+
+    void deallocate(T* p, std::size_t size) noexcept {
+        alloc.deallocate(p, size);
+    }
 
 private:
+    base_allocator alloc;
     profile_state& state;
     // We want to call the underlying allocator type
 };
 
-/// Offer equivalent pmr allocator types
-namespace pmr {
+/// Offer equivalent STL container types.
+template<typename T, typename Alloc=std::allocator<T>>
+using vector = std::vector<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using vector = std::vector<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using deque = std::deque<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using deque = std::deque<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using forward_list = std::forward_list<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using forward_list = std::forward_list<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using list = std::list<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using list = std::list<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using set = std::set<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using set = std::set<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using map = std::map<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using map = std::map<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using multiset = std::multiset<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using multiset = std::multiset<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using multimap = std::multimap<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using multimap = std::multimap<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using unordered_set = std::unordered_set<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using unordered_set = std::unordered_set<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using unordered_map = std::unordered_map<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using unordered_map = std::unordered_map<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using unordered_multiset = std::unordered_multiset<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using unordered_multiset = std::unordered_multiset<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using unordered_multimap = std::unordered_multimap<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using unordered_multimap = std::unordered_multimap<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
+template<typename T, typename Alloc=std::allocator<T>>
+using stack = std::stack<T, profile_allocator<T, Alloc>>;
 
-template<typename T>
-using stack = std::stack<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
-
-template<typename T>
-using queue = std::queue<T, profile_allocator<T, std::pmr::polymorphic_allocator<T>>>;
-
-} // namespace pmr
+template<typename T, typename Alloc=std::allocator<T>>
+using queue = std::queue<T, profile_allocator<T, Alloc>>;
 
 } // namespace erata
